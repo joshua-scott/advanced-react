98c28cb3abc5e283bdbf9a0cc139f7f2aca17991
Joshua Scott <jscott313@gmail.com>
2019-03-23 14:41:19 +0200

    Add query to get all items, and mutation to add an item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,15 @@
 const Mutation = {
-  createDog(parent, args, ctx, info) {
-    // Soon, we will save to a database using Prisma
-    // GraphQL Yoga is a server, and doesn't have a database on its own
-    // But let's use in-memory global as our database for now,
-    // just to show how it works
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // todo: check they're logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args },
+      },
+      info
+    );
+
+    return item;
   },
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,7 +1,16 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    return global.dogs || [];
-  },
+  // This is the 'standard' way of making a query to our Prisma db:
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+
+  // ...but in a common case where the query is exactly the same as we have in Prisma,
+  // and we don't need anything else to happen such as authentication etc,
+  // we can simply forward the query directly to our Prisma db, like this:
+  items: forwardTo('db'),
 };
 
 module.exports = Query;
