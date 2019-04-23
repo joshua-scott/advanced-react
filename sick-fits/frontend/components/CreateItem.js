db4874e4b7f1b4e61ff7b43e88c4e204676969e9
Joshua Scott <jscott313@gmail.com>
2019-04-23 15:07:51 +0300

    Add ability to add images

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -43,6 +43,27 @@ class CreateItem extends Component {
     });
   };
 
+  uploadFile = async e => {
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/joshua-scott/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const file = await res.json();
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     const { title, price, description, image, largeImage } = this.state;
 
@@ -61,6 +82,18 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <img src={image} alt="Upload preview" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
