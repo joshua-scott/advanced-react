b54df8b2d67408aac9f7b02c7be525b7e506b38d
Joshua Scott <jscott313@gmail.com>
2019-06-04 11:09:05 +0300

    Identify the currently logged in user (backend)

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,7 +8,16 @@ const server = createServer();
 
 server.express.use(cookieParser());
 
-// todo: Use express middleware to populate current user
+// Decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
