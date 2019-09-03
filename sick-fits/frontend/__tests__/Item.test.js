ad74e94d2c742be312f43ff3dc7d9b478e8bb7d1
Joshua Scott <jscott313@gmail.com>
2019-09-03 19:40:03 +0300

    Use a snapshot test for Item.js instead

--- sick-fits/frontend/__tests__/Item.test.js
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import ItemComponent from '../components/Item';
 
 const fakeItem = {
@@ -11,26 +13,8 @@ const fakeItem = {
 };
 
 describe('<Item />', () => {
-  // Note: This isn't necessarily the best approach to testing this component
-  // Snapshots might be better
-  it('renders the image properly', () => {
+  it('renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const img = wrapper.find('img');
-    expect(img.props().src).toBe(fakeItem.image);
-    expect(img.props().alt).toBe(fakeItem.title);
-  });
-  it('renders the price tag and title properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const PriceTag = wrapper.find('PriceTag');
-    expect(PriceTag.children().text()).toBe('€50');
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
-  });
-  it('renders the buttons properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const buttonList = wrapper.find('.buttonList');
-    expect(buttonList.children()).toHaveLength(3);
-    expect(buttonList.find('Link')).toHaveLength(1);
-    expect(buttonList.find('AddToCart')).toHaveLength(1);
-    expect(buttonList.find('DeleteItem')).toHaveLength(1);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
